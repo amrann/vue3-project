b991f1117a7f0da3a31e7dac82fcf070c51897a1
meran <amr.bin.an@gmail.com>
2024-04-14 04:28:47 +0700

    feat : adding top categories on homepage and installing axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Headline from './../components/Headline.vue'
+import Categories from './../components/Categories.vue'
 
 </script>
 
 <template>
   <main>
     <Headline />
+    <Categories />
   </main>
 </template>
