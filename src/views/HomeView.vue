3ad26bd2d6a0a32b1dd7b6a332b047f751062378
meran <amr.bin.an@gmail.com>
2024-04-14 14:37:32 +0700

    feat : adding items section on homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Headline from './../components/Headline.vue'
 import Categories from './../components/Categories.vue'
+import Items from './../components/Items.vue'
 
 </script>
 
@@ -8,5 +9,6 @@ import Categories from './../components/Categories.vue'
   <main>
     <Headline />
     <Categories />
+    <Items />
   </main>
 </template>
